87d94e030b7d7edfad08b00b6080a71f95857de6
“gevera” <skryonline@gmail.com>
2024-05-25 15:45:19 +0300

    Fix env

--- houdini.config.js
+++ houdini.config.js
@@ -1,15 +1,13 @@
 /// <references types="houdini-svelte">
 
-import 'dotenv/config'
-
 /** @type {import('houdini').ConfigFile} */
 const config = {
-    "watchSchema": {
-        "url": process.env.GRAPHQL_ENDPOINT
-    },
-    "plugins": {
-        "houdini-svelte": {}
-    }
+  "watchSchema": {
+    "url": 'env:GRAPHQL_ENDPOINT'
+  },
+  "plugins": {
+    "houdini-svelte": {}
+  }
 }
 
 export default config
